1b6951f891269c323aa22b871c813bc74adb498b
natoles <lindien.martin@gmail.com>
2019-10-23 01:49:16 -0400

    added checks

--- app/src/main/java/com/example/mapstest/MapsActivity.java
+++ app/src/main/java/com/example/mapstest/MapsActivity.java
@@ -1,71 +1,108 @@
 package com.example.mapstest;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 import android.app.Dialog;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
-    GoogleApiClient mGoocleApiClient;
+    private static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final int DEFAULT_ZOOM = 15;
+    private static final String TAG = "MapsActivity";
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private Boolean mLocationPermissionsGranted = false;
+    private FusedLocationProviderClient mFusedLocationprividerClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps2);
-        if(googleServicesAvailable()){
-            Toast.makeText(this, "Noice", Toast.LENGTH_LONG).show();
+        if(isServicesOK()){
+            //Toast.makeText(this, "Services OK", Toast.LENGTH_LONG).show();
         }
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        //mMap.setMyLocationEnabled(true);
+        goToLocation(50,50);
 
-        goToLocation(48.420089, -71.052202, 15);
+    }
 
-        // Add a marker in Sydney and move the camera
 
-        /*LatLng sydney = new LatLng(0, 0);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+    private void getDeviceLocation()
+    {
+        Log.d(TAG, "getDeviceLocation");
+
+        mFusedLocationprividerClient = LocationServices.getFusedLocationProviderClient(this);
+        try{
+            if (mLocationPermissionsGranted){
+                Task location = mFusedLocationprividerClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAG, "getDeviceLocation : Success");
+                            Location currentLocation = (Location) task.getResult();
+                            goToLocation(currentLocation.getLatitude(), currentLocation.getLongitude());
+                        } else {
+                            Log.d(TAG, "getDeviceLocation : can't find location");
+                            Toast.makeText(MapsActivity.this, "Can't get current location !", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        }catch (SecurityException e){
+            Log.e(TAG, "getDeviceLocation : SecurityException " + e.getMessage());
+        }
+
+    }
+
+
+
+
+    private void initMap(){
+        Log.d(TAG, "initMap: initializing map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
     }
 
     private void goToLocation(double lat, double lng)
     {
         LatLng position = new LatLng(lat, lng);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(DEFAULT_ZOOM));
     }
 
     private void goToLocation(double lat, double lng, float zoom)
@@ -75,24 +112,73 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.zoomTo(zoom));
     }
 
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: getting location permissions");
+        String[] permissions = {FINE_LOCATION, COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionsGranted = true;
+            }else{
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }else{
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE); //Lauches onRequestPermissionsResult
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: called.");
+        mLocationPermissionsGranted = false;
+
+        switch(requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if(grantResults.length > 0){
+                    for(int i = 0; i < grantResults.length; i++){
+                        if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionsGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                    mLocationPermissionsGranted = true;
+                    //initialize our map
+                    initMap();
+                }
+            }
+        }
+    }
+
     //Tells if the app can have access to google play services, required
     //for google maps to work
-    public boolean googleServicesAvailable (){
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK()");
         GoogleApiAvailability api = GoogleApiAvailability.getInstance();
         int isAvailable = api.isGooglePlayServicesAvailable(this);
         if (isAvailable == ConnectionResult.SUCCESS)
         {
+            Log.d(TAG, "isServicesOK() : Success");
             return true;
         }
         else
         {
             if (api.isUserResolvableError(isAvailable))
             {
-                Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+                Log.d(TAG, "isServicesOK() : resolvable");
+                Dialog dialog = api.getErrorDialog(this, isAvailable, ERROR_DIALOG_REQUEST);
                 dialog.show();
             }
             else
             {
+                Log.d(TAG, "isServicesOK() : Failure");
                 Toast.makeText(this, "I can't connect to Google Play services !", Toast.LENGTH_LONG).show();
             }
         }
