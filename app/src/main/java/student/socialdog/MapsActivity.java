8086cd107e83e81539ad02f85f5aa370ea565a87
Thomas Gabriel <55448922+thomsgab@users.noreply.github.com>
2019-12-06 14:02:16 -0500

    Firebase (#4)
    firebase r+w friends & dogs & markers OK, markers approb OK

--- app/src/main/java/student/socialdog/MapsActivity.java
+++ app/src/main/java/student/socialdog/MapsActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.Context;
@@ -18,7 +17,6 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -30,29 +28,24 @@ import android.app.Dialog;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 
 public class MapsActivity extends Fragment implements OnMapReadyCallback, LocationListener {
@@ -65,13 +58,13 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
     private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private Boolean mLocationPermissionsGranted = false;
+    static boolean verifyOK = false;
+    static boolean goForVerify = false;
     Bitmap bagIcon;
     Bitmap treeIcon;
     LatLng currentLocation;
-    ArrayList<JSONObject> markersList;
-
+    DatabaseReference markersDB, markers_unapprovedDB, allMarkersDB;
     LocationManager locationManager;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -91,6 +84,9 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
         BitmapDrawable treeBitmap=(BitmapDrawable)getResources().getDrawable(R.drawable.arbre);
         treeIcon = Bitmap.createScaledBitmap(treeBitmap.getBitmap(), width, height, false);
 
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        markersDB = database.getReference("markers");
+
         Button addmarker = rootView.findViewById(R.id.addmarker);
 
         addmarker.setOnClickListener(new View.OnClickListener() {
@@ -99,8 +95,8 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
                 addMarkerAtCurrentLocation(v);
             }
         });
-        JSONObject list_markers = assetLoader.JSON(getContext(),"markers.json");
-        markersList = assetLoader.getJSONArray(list_markers, "markers");
+
+
 
         return rootView;
     }
@@ -109,45 +105,44 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        Log.e(TAG, "onMapReady: "+markersList.toString());
 
-        double lati;
-        double longi;
-        String type;
-        Bitmap markerIcon;
-
-        try{
-            JSONObject obj = new JSONObject();
-            obj.put("type", "bag");
-            obj.put("latitude", 20);
-            obj.put("longitude", 20);
-            markersList.add(obj);
-        } catch(Exception ex){
-            ex.printStackTrace();
-            return;
-        }
-
-
-
-
-        for(int i = 0; i<markersList.size(); i++)
-        {
-            try{
-                type = markersList.get(i).getString("type");
-                lati = markersList.get(i).getDouble("latitude");
-                longi = markersList.get(i).getDouble("longitude");
-            } catch(Exception ex){
-                ex.printStackTrace();
-                return;
+        // Lecture des marqueurs depuis database FireBase
+        markersDB.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                HashMap<String, Map> markers =  (HashMap<String,Map>)dataSnapshot.getValue();
+                Bitmap markerIcon;
+                Object lati;
+                Object longi;
+                Object type;
+                for(Map.Entry<String, Map> current : markers.entrySet())
+                {
+                    try{
+                        type = current.getValue().get("type");
+                        lati = current.getValue().get("latitude");
+                        longi = current.getValue().get("longitude");
+                    } catch(Exception ex){
+                        ex.printStackTrace();
+                        return;
+                    }
+                    if ("bag".equals(type)){
+                        markerIcon = bagIcon;
+                    }
+                    else{
+                        markerIcon = treeIcon;
+                    }
+                    mMap.addMarker(new MarkerOptions()
+                            .position(new LatLng((Double)lati, (Double) longi))
+                            .icon(BitmapDescriptorFactory.fromBitmap(markerIcon))
+                            .title(getAddress((Double)lati, (Double) longi)));
+                }
             }
-            if ("bag".equals(type)) markerIcon = bagIcon;
-            else markerIcon = treeIcon;
 
-            mMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(lati, longi))
-                    .icon(BitmapDescriptorFactory.fromBitmap(markerIcon))
-                    .title(getAddress(lati, longi)));
-        }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        });
 
 
         if(mLocationPermissionsGranted){
@@ -155,7 +150,6 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
             goToLocation(currentLocation.latitude, currentLocation.longitude);
             mMap.setMyLocationEnabled(true);
             mMap.getUiSettings().setCompassEnabled(true);
-
         }
     }
 
@@ -255,7 +249,17 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
 
     public void addMarkerAtCurrentLocation(View view)
     {
-        Log.e(TAG, "Hello there");
+        /***
+         * Un marqueur ne sera pas directement approuvé : il ira dans une base de marqueurs
+         * non approuvés.
+         * Une fois qu'il y a 2 marqueurs non approuvés très proches, ils sont réunis
+         * en un seul marqueur approuvé.
+         * C'est une méthode simpliste de vérification pour de l'informatique participative.
+         * Voir procédure verifyMarkersUnapproved()
+         */
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        markers_unapprovedDB = database.getReference("markers_unapproved").push();
         getLocation();
         String[] markersTypes = {"Espace vert", "Sac à déjection"};
 
@@ -265,28 +269,39 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
 
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                // the user clicked on markersTypes[which]
                 Bitmap markerIcon;
-                String markerTitle;
+                String type="";
                 switch(which){
                     case(0):
                     default:
                         markerIcon = treeIcon;
-                        markerTitle = "Espace vert";
+                        type = "tree";
                         break;
                     case(1):
                         markerIcon = bagIcon;
-                        markerTitle = "Sac à déjection";
+                        type = "bag";
                         break;
                 }
-                MarkerOptions options = new MarkerOptions()
-                        .position(new LatLng(currentLocation.latitude, currentLocation.longitude))
-                        .title(getAddress(currentLocation.latitude, currentLocation.longitude))
-                        .icon(BitmapDescriptorFactory.fromBitmap(markerIcon));
-                mMap.addMarker(options);
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("latitude", currentLocation.latitude);
+                map.put("longitude", currentLocation.longitude);
+                map.put("type", type);
+
+                verifyMarkersUnapproved(currentLocation.latitude,currentLocation.longitude, type);
+                while(goForVerify==false);
+                if(verifyOK){
+                    // Ajout à la DB du nouveau marqueur
+                    markersDB.setValue(map);
+                }
+                else{
+                    markers_unapprovedDB = database.getReference("markers_unapproved").push();
+                    markers_unapprovedDB.setValue(map);
+                }
             }
+
         });
         builder.show();
+
     }
 
     private void getLocationPermission(){
@@ -361,6 +376,51 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
             }
         }
         return false;
+    }
+
+
+    public void verifyMarkersUnapproved(double latiReceived, double longiReceived, final String typeReceived){
+        final double curLati = (double)Math.round((double)latiReceived * 100000d) / 100000d;
+        final double curLongi = (double)Math.round((double)longiReceived * 100000d) / 100000d;
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        markers_unapprovedDB = database.getReference("markers_unapproved");
+        markersDB = database.getReference("markers").push();
+        markers_unapprovedDB.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                HashMap<String, Map> markers_unapproved =  (HashMap<String,Map>)dataSnapshot.getValue();
+                HashMap<String, Map> markers_trunc = new HashMap<>();
+                HashMap<String, Double> map = new HashMap<>();
+                // On ajoute tous les objets dans une nouvelle map, avec
+                // les coordonnées arrondies pour les comparer
+                for(Map.Entry<String, Map> current : markers_unapproved.entrySet())
+                {
+                    Object key = current.getKey();
+                    Object lati = current.getValue().get("latitude");
+                    Object longi = current.getValue().get("longitude");
+                    lati = (double)Math.round((double)lati * 100000d) / 100000d;
+                    longi = (double)Math.round((double)longi * 100000d) / 100000d;
+
+                    map.put("latitude",(double) lati);
+                    map.put("longitude", (double) longi);
+                    markers_trunc.put(key.toString(),map);
+                }
+                // On compare nos coordonnées arrondies
+                for(Map.Entry<String, Map> current : markers_trunc.entrySet())
+                {
+                    if((double)current.getValue().get("longitude") == curLongi
+                    && (double)current.getValue().get("latitude") == curLati){
+                        verifyOK = true;
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+        goForVerify = true;
     }
 
 
--- app/src/main/java/student/socialdog/assetLoader.java
+++ app/src/main/java/student/socialdog/assetLoader.java
@@ -1,12 +1,16 @@
 package student.socialdog;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.util.Log;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class assetLoader {
     private static Context mContext = null;
@@ -59,8 +63,8 @@ public class assetLoader {
      * Returns an ArrayList of FriendAdapter.FriendsObject
      * @return ArrayList<FriendAdapter.FriendsObject> Return a list of FriendAdapter.FriendsObject
      */
-    public static ArrayList<FriendAdapter.FriendsObject> getFriends(){
-        return getFriends(mContext);
+    public static ArrayList<FriendAdapter.FriendsObject> getFriends(HashMap<String,Map> friendsInDB){
+        return getFriends(mContext,friendsInDB);
     }
 
     /**
@@ -68,27 +72,22 @@ public class assetLoader {
      * @param pcontext Context to use to load JSON file. Might be usefull if mContext from assetLoader isn't initialized yet
      * @return ArrayList<FriendAdapter.FriendsObject> Return a list of FriendAdapter.FriendsObject
      */
-    public static ArrayList<FriendAdapter.FriendsObject> getFriends(Context pcontext){
-        JSONObject jsonObject;
-        if(pcontext!=null){
-            jsonObject = assetLoader.JSON(assetLoader.mContext,"friends.json");
-        }else{
-            Log.e("assetLoader","Context isn't loaded yet, assetLoader.JSON or getResIDfromImageName(String imageName,Context context) to load context !");
-            return null;
-        }
-        ArrayList<FriendAdapter.FriendsObject> friends = new ArrayList<>();
-        try{
-            JSONArray jsonArrayFriends = jsonObject.getJSONArray("friends");
-            for (int i = 0; i < jsonArrayFriends.length();i++){
-                JSONObject currentFriend = jsonArrayFriends.getJSONObject(i);
-                int imgResID = getResIDfromImageName(currentFriend.getString("ppic"),pcontext);
-                friends.add(new FriendAdapter.FriendsObject(currentFriend.getString("name"),currentFriend.getString("lastWalk"),imgResID));
+
+    public static ArrayList<FriendAdapter.FriendsObject> getFriends(Context pcontext, HashMap<String,Map> friendsInDB) {
+            ArrayList<FriendAdapter.FriendsObject> friendslist = new ArrayList<>();
+        for(Map.Entry<String, Map> current : friendsInDB.entrySet()) {
+            try{
+
+                Object name = current.getValue().get("name");
+                Object lastWalk = current.getValue().get("lastWalk");
+                Object ppic = current.getValue().get("ppic");
+                int imgResID = getResIDfromImageName(ppic.toString(),pcontext);
+                friendslist.add(new FriendAdapter.FriendsObject((String) name,(String) lastWalk, imgResID));
+            } catch(Exception ex){
+                ex.printStackTrace();
             }
-            return friends;
-        }catch(Exception ex){
-            ex.printStackTrace();
-            return null;
         }
+        return friendslist;
     }
 
     /**
@@ -96,7 +95,7 @@ public class assetLoader {
      * @param imageName Name of the image ressource to load.
      * @return int Return the ressource ID of image named "imageName" as an "int"
      */
-    private static int getResIDfromImageName(String imageName){
+    public static int getResIDfromImageName(String imageName){
         int result = getResIDfromImageName(imageName,mContext);
         if(result == -1){
             Log.e("assetLoader","Context isn't loaded yet, assetLoader.JSON or getResIDfromImageName(String imageName,Context context) to load context !");
@@ -117,4 +116,40 @@ public class assetLoader {
         Log.e("assetLoader","Context parameter isn't correct.");
         return -1;
     }
+
+
+    /**
+     * Returns an ArrayList of DogAdapter.DogObject
+     * @return ArrayList<DogAdapter.DogObject> Return a list of DogAdapter.DogObject
+     */
+    public static ArrayList<DogAdapter.DogObject> getDogs(Map<String,Map> dogsInDB){
+        return getDogs(mContext,dogsInDB);
+    }
+
+    /**
+     * Returns an ArrayList of DogAdapter.DogObject
+     * @param pcontext Context to use to load JSON file. Might be usefull if mContext from assetLoader isn't initialized yet
+     * @return ArrayList<DogAdapter.DogObject> Return a list of DogAdapter.DogObject
+     */
+
+    public static ArrayList<DogAdapter.DogObject> getDogs(Context pcontext,Map<String,Map> dogsInDB) {
+        ArrayList<DogAdapter.DogObject> dogslist = new ArrayList<>();
+        for(Map.Entry<String, Map> current : dogsInDB.entrySet()) {
+            try{
+                Object dogname = current.getValue().get("dogname");
+                Object dograce = current.getValue().get("dograce");
+                Object dogage = current.getValue().get("dogage");
+                Object lastWalk = current.getValue().get("lastWalk");
+                Object dogcolor = current.getValue().get("dogcolor");
+                int idogcolor = Integer.parseInt(dogcolor.toString());
+                Object dogpic = current.getValue().get("dogpic");
+                int imgResID = getResIDfromImageName(dogpic.toString(),pcontext);
+                dogslist.add(new DogAdapter.DogObject((String) dogname,(String) dograce,(String) dogage,(String) lastWalk, idogcolor,imgResID));
+            } catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+        return dogslist;
+    }
+
 }
