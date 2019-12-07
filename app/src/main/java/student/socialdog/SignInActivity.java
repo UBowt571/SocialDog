d9a7a31ae290842c84eedb108995bfc6a305d3f1
Thomas GABRIEL <thom.gbl@gmail.com>
2019-12-07 11:07:05 -0500

    Google connection caught in Firebase console !

--- app/src/main/java/student/socialdog/SignInActivity.java
+++ app/src/main/java/student/socialdog/SignInActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -13,12 +15,20 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
     GoogleSignInClient mGoogleSignInClient;
     int RC_SIGN_IN = 32;
+    private FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +36,13 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_signin);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build();
 
         // Création du GoogleSignInClient avec les options de la variable gso (email uniquement).
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        fAuth = FirebaseAuth.getInstance();
 
     }
 
@@ -38,8 +50,8 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     protected void onStart(){
         super.onStart();
         // On vérifie si l'utilisateur n'est pas déjà connecté avec son compte google
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        signInOK(account);
+        FirebaseUser currentUser = fAuth.getCurrentUser();
+        signInOK(currentUser);
     }
 
     @Override
@@ -63,24 +75,19 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         // Result de l'intent SingInIntent
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                // Google Sign In OK, Authentification Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Echec connexion google
+                Log.w("", "Google sign in failed", e);
+            }
+
         }
     }
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            signInOK(account); // Connexion réussie, on se dirige vers le main activity
-
-        } catch (ApiException e) {
-            // ApiException détaille la raison de l'échec.
-            // Voir classe GoogleSignInStatusCodes pour plus d'infos.
-            Log.w("Erreur", "signInResult:failed code=" + e.getStatusCode());
-            signInOK(null);
-        }
-    }
-
-    private void signInOK(GoogleSignInAccount account){
+    private void signInOK(FirebaseUser account){
         // On lance la main activity si il est déjà connecté
         if(account != null) {
             startActivity(new Intent(this, MainActivity.class));
@@ -91,6 +98,29 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
             signInButton.setSize(SignInButton.SIZE_STANDARD);
             signInButton.setOnClickListener(this);
         }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        fAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("","signInWithCredential:success");
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            signInOK(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(getApplicationContext(),"Echec de la connexion",Toast.LENGTH_SHORT).show();
+                            signInOK(null);
+                        }
+                    }
+                });
     }
 
 
